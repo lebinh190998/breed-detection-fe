0bb9e19d7704cece9d1de10411a38fbbe45d9476
Binh Le <lebinh190998@gmail.com>
2023-07-18 20:52:57 +0700

    initial

---
+++ app/posts/new/error.tsx
@@ -0,0 +1,27 @@
+'use client'; // Error components must be Client components
+import React, {useEffect} from 'react';
+
+
+type Props = {
+    error: string;
+    reset: () => void;
+}
+
+const error = ({error, reset}: Props) => {
+    useEffect(() => {
+        // Log the error to an error reporting service
+        console.error(error)
+    }, [error])
+  
+    return (
+    <div>
+        <h2>Something went wrong</h2>
+        <button
+         onClick={() => reset()}>
+            Try again
+        </button>
+    </div>
+  )
+}
+
+export default error
